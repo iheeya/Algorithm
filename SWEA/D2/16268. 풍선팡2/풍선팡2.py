79f44acf7aafbc5668c3113383f5cb2f468970eb
hee <149504381+iheeya@users.noreply.github.com>
2024-02-02 23:01:17 +0900

    [D2] Title: 풍선팡2, Time: 169 ms, Memory: 59,992 KB -BaekjoonHub

---
+++ SWEA/D2/16268. 풍선팡2/풍선팡2.py
@@ -0,0 +1,30 @@
+T = int(input())
+
+for test_case in range(1, T+1):
+    N, M = map(int, input().split())
+    #N: 행   M: 열
+    flower = [list(map(int, input().split())) for _ in range(N)]
+            
+    # 2차원 배열의 처음부터 끝까지 순회하면서 본인, 상하좌우의 합을 구해서
+    # 리스트에 그 값을 넣는다
+    # (이때 상하좌우 중 좌표가 존재하지 않는 곳이 있다면 0을 더하거나 무시)
+    
+    # 2차원 배열의 순회를 마치면 배열에 저장된 가장 큰 값을 출력
+    
+    di = [0, 1, 0, -1]    #주변을 탐색하기 위한 좌표 계산값
+    dj = [1, 0, -1, 0]
+    max_flower = []
+    
+    for i in range(N):
+        for j in range(M):
+            total = 0 
+            for k in range(4):   #탐색할 4개의 칸에 대해
+                ni = i + di[k]   #탐색하고자 하는 좌표로 가기
+                nj = j + dj[k]
+                if 0<= ni < N and 0<=nj<M:   # 좌표 범위 안에 있을때만 배열에 접근하도록 한다. 
+                    total +=  flower[ni][nj] 
+                    
+            total += flower[i][j]      # 본인 숫자도 더하기
+            max_flower.append(total)   # 계산 결과 저장
+    
+    print(f'#{test_case} {max(max_flower)}')
