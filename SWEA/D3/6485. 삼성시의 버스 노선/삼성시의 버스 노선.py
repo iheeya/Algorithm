e4e736f541294d691a9668fd914fcc7a9410de78
hee <149504381+iheeya@users.noreply.github.com>
2024-02-21 17:29:55 +0900

    [D3] Title: 삼성시의 버스 노선, Time: 206 ms, Memory: 61,576 KB -BaekjoonHub

--- SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.py
+++ SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.py
@@ -1,20 +1,21 @@
-T = int(input())   #테스트 케이스
+
+T = int(input())
 
 for test_case in range(1, T+1):
-    bus_stops = [0] * 5001    # 삼성시의 버스 정류장 번호
+    N = int(input())   # 버스 노선의 수
 
-    N = int(input())   # 버스 노선 N개
+    # 출력: P개의 버스 정류장에 대해 각 정류장에 몇개의 버스 노선이 다니는가?
+    result = []  # 결과를 저장할 리스트
+    bus_stop = [0] * 50001  # 인덱스 번호 == 버스 정류장번호 (1 ~5000)
 
-    for stops in range(N):
-        A, B = map(int, input().split())
-        for stop in range(A, B+1):
-            bus_stops[stop] += 1
+    for i in range(N):
+        A, B = map(int, input().split())  # A이상, B이하인 버스정류장만을 다니는 버스 노선
+        for j in range(A, B+1):
+            bus_stop[j] += 1
 
-    P = int(input())
-    result = []
-
-    for c in range(P):
+    P = int(input())  # 버스 정류장의 수
+    for i in range(P):
         C = int(input())
-        result.append(bus_stops[C])
-
+        result.append(bus_stop[C])
+    
     print(f'#{test_case}', *result)
