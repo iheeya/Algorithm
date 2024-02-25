7304a938a8b903a4f5a43e23336173ae8575388e
hee <149504381+iheeya@users.noreply.github.com>
2024-02-25 23:01:34 +0900

    [D2] Title: 파리 퇴치, Time: 122 ms, Memory: 52,988 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,19 +1,22 @@
+
 T = int(input())
 
 for test_case in range(1, T+1):
-    N, M = map(int, input().split())   # N: 배열의 크기  M: 파리채의 크기
-    my_matrix = [list(map(int, input().split())) for _ in range(N)]
-    
-    max = 0   #최대값
-    
-    for row in range(N-M+1):      #확인해야 할 개수의 시작 좌표
-        for col in range(N-M+1):
-            cnt = 0     # 파리채로 죽인 파리의 개수를 저장할 변수
-            for x in range(row, row+M):   #시작좌표에서 부터 파리채의 크기만큼
-                for y in range(col, col+M):
-                    cnt += my_matrix[x][y]   # 파리채의 크기 안에서 죽은 파리의 개수
-                    
-            if cnt > max:
-                max = cnt
-                
-    print(f'#{test_case} {max}')
+    N, M = map(int, input().split())
+    # N: 배열의 길이, M: 파리채의 크기
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    mx = 0  # 가장 많이 죽인 파리의 수
+
+
+    # 파리채를 통해 가장 많이 죽은 파리는?
+    for i in range(N-M+1):   # 파리채가 이동할 수 있는 시작지점만큼 반복
+        for j in range(N-M + 1):
+            cnt = 0  #죽인 파리의 개수
+            for k in range(i, M+i):
+                for l in range(j, M+j):
+                    cnt += arr[k][l]
+            
+            if cnt > mx:
+                mx = cnt
+
+    print(f'#{test_case} {mx}')            
