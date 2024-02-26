d5c0e22eada7404bf9ded070bf757a765733a68b
hee <149504381+iheeya@users.noreply.github.com>
2024-02-26 16:59:58 +0900

    [D3] Title: 퍼펙트 셔플, Time: 169 ms, Memory: 61,468 KB -BaekjoonHub

--- SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.py
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.py
@@ -1,28 +1,23 @@
+def get_result():
+    # 투포인트 알고리즘에서 point1 = 맨 처음, point2 = 중간 +1
+    pointer1 = 0
+    pointer2 = (N + 1) // 2
+
+    for turn in range(N):
+        if turn % 2 == 0:
+            print(str[pointer1], end=' ')
+            pointer1 += 1
+        else:
+            print(str[pointer2], end=' ')
+            pointer2 += 1
+
+
 T = int(input())
 
 for test_case in range(1, T+1):
     N = int(input())   # 카드의 개수
-    cards = list(map(str, input().split()))
-    cal = N // 2   # 카드의 개수를 반으로 나눈값
-    a = []     # 카드를 반씩 잘라서 저장할 리스트
-    b = []
+    str = list(input().split())
 
-    if N % 2 == 0:
-        a = cards[:cal]
-        b = cards[cal:]
-    else:
-        # 홀수인 카드뭉치는 앞쪽의 카드가 한장 더 갖도록 한다.
-        a = cards[: cal + 1]
-        b = cards[cal+1:]
-
-    result = []   # 결과를 저장할 리스트
-    result.append(a[0])   # 먼저 들어가야할 단어 append
-
-    for i in range(1, N):
-        idx = i//2     # 자른 리스트 a, b의 인덱스에 접근하기 위해 연산 필요
-        if i % 2 != 0:
-            result.append(b[idx])
-        else:
-            result.append(a[idx])
-
-    print(f'#{test_case}', *result)
+    print(f'#{test_case}', end = ' ')
+    get_result()
+    print()   # 케이스가 출력될때마다 줄바꿈
