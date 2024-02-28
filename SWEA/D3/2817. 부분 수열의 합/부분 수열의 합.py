1a67135cfd523d33c823abf598eff88f19b29196
hee <149504381+iheeya@users.noreply.github.com>
2024-02-28 12:01:29 +0900

    [D3] Title: 부분 수열의 합, Time: 7,683 ms, Memory: 60,840 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,24 +1,19 @@
 T = int(input())
 
 for test_case in range(1, T+1):
-    N, K = map(int, input().split())
-    # N: 자연수 수열의 길이
-    # K: 찾고자 하는 합
-    A = list(map(int, input().split()))   # 수열
+    N, K = map(int, input().split())   # N: 수열의 개수 K: 구하고자 하는 합
+    A = list(map(int, input().split()))    # N개의 자연수 수열
+    cnt = 0  # 합이 K인 부분집합의 개수를 저장할 변수
 
-    cnt = 0  # N개의 원소를 가지고 있고, 원소의 합이 K인 부분집합의 개수
 
-    for i in range(1 << len(A)):  # 2의 12승개에 해당되는 요소
-        subset = []  # 부분집합 원소를 저장할 리스트
-        for j in range(len(A)):  # 해당 i의 j번째 비트가 1인지 확인
-            if i & (1 << j):  # 비트가 1 이라면 부분집합에 포함되는 요소 (i와 (1<<j)의 & 비트연산의 결과가 참이라면)
-                subset.append(A[j])  # 비트가 1 이라면 부분집합 리스트에 append
+    # 비트연산자를 이용해 부분집합 만들기
+    for i in range(1<<N):
+        subset = []
+        for j in range(N):
+            if i & (1<<j):
+                subset.append(A[j])
+        if sum(subset) == K:  # 부분집합의 원소들의 합이 K라면 cnt += 1
+            cnt += 1
 
-        if sum(subset) == K:  # 부분집합 원소의 합이 K라면
-            cnt += 1  # 부분집합의 개수 1 더하기
-
-        # 중첩 조건문 하나로 쓸 수도 있음
-        # if len(subset) == N and sum(subset) == K:
-        #     num_subset += 1
-
+    # 출력: 합이 K가 되는 경우의 수
     print(f'#{test_case} {cnt}')
