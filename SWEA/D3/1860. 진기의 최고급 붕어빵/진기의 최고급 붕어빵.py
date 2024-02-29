d27c89fbd4d59d6291792868b9294de1b52a1014
hee <149504381+iheeya@users.noreply.github.com>
2024-02-29 11:17:31 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 423 ms, Memory: 64,116 KB -BaekjoonHub

--- SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
@@ -2,20 +2,24 @@ T = int(input())
 
 for test_case in range(1, T+1):
     N, M, K = map(int, input().split())
-    # N: 붕어빵을 살 수 있는 사람
-    # M: 진기가 붕어빵을 만드는 시간  (M초의 시간을 들이면 K개의 붕어빵을 만들 수 있다.)
-    # K: 만든 붕어빵의 개수
-    nums = list(map(int, input().split()))   # 사람들이 언제 도착하는지 나타내는 초 단위(한 사람씩 따로 온다.)
+    # N: 붕어빵을 살 수 있는 사람들  M: 붕어빵을 만드는데 걸리는 시간  K: 만들 수 있는붕어빵의 개수
+    customer = list(map(int, input().split()))   # N명의 손님이 도착하는 시간
+    customer.sort()   # 손님이 도착하는 시간을 빠른 순대로 정렬
 
-    result = 'Possible'   # 진기가 손님을 기다리게 하지 않고 붕어빵을 줄지 안줄지
+    sold = 0   # 지금까지 팔린 빵의 개수
+    sol = 'Possible'   # 진기가 빵을 팔수 있는지 없는지
 
-    # 손님이 오는 시간 순대로 정렬
-    nums.sort()
+    for p in customer:
+        make_bread = (p // M) * K
 
-    # 진기가 n초 안에 만들 수 있는 붕어빵의 개수보다 손님이 오는 시간이 더 빠르다면 불가능
-    for i in range(N):   # 붕어빵을 살 수 있는 사람의 수 만큼 반복
-        b = (nums[i]//M) * K  # nums[i]초 까지 만들 수 있는 붕어빵의 개수
-        if b < i+1:  # 붕어빵의 개수가 손님의 수보다 작으면
-            result = 'Impossible'  # 붕어빵을 줄 수 없다.
+        # 판 빵의 개수
+        sold += 1
 
-    print(f'#{test_case} {result}')
+        # 남은 빵의 개수
+        remain = make_bread - sold
+        # 남은 빵의 개수가 0보다 작다면 더 이상 손님에게 팔 수 있는 붕어빵이 없다는 것
+        if remain < 0:
+            sol = 'Impossible'
+
+    print(f'#{test_case} {sol}')
+
