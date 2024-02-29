e692f6f06d252f04f2408ff80f67652766eb2265
hee <149504381+iheeya@users.noreply.github.com>
2024-02-29 10:09:27 +0900

    [D3] Title: 세제곱근을 찾아라, Time: 163 ms, Memory: 45,564 KB -BaekjoonHub

--- SWEA/D3/5688. 세제곱근을 찾아라/세제곱근을 찾아라.py
+++ SWEA/D3/5688. 세제곱근을 찾아라/세제곱근을 찾아라.py
@@ -1,25 +1,17 @@
-
 T = int(input())
 
 for test_case in range(1, T+1):
-    N = int(input())
+    N = int(input())    # 세제곱 결과가 N이 나오게 되는 수를 구하라
 
-    # 출력: N == x^3 이되는 양의 정수 x , 존재하지 않으면 -1 출력
-    
-    '''
-    N의 제곱근 : N ** (1/2)
-    제곱근을 구하기 위해서는 해당 {1/제곱의 수}로 계산할 수 있다.
-    '''
-    # N의 세제곱근
-    num = N ** (1/3)
+    num = 1      # 세제곱할 수
+    result = 0   # 세 제곱근을 저장할 수
 
-    # result 반올림,
-    result = round(num)
-
-    sol = result ** 3
-
-    if N == sol:
-        print(f'#{test_case} {result}')
-    # 안되면 -1반환환
+    while result < N:
+        result = num ** 3
+        if result == N:
+            break
+        num += 1
     else:
-        print(f'#{test_case} -1')
+        num = -1
+
+    print(f'#{test_case} {num}')
