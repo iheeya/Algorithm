4cd86e133b976dd5192ea7bd552368896f4e5901
hee <149504381+iheeya@users.noreply.github.com>
2024-03-03 16:40:57 +0900

    [Silver IV] Title: 수열, Time: 88 ms, Memory: 32684 KB -BaekjoonHub

--- 백준/Silver/2491. 수열/수열.py
+++ 백준/Silver/2491. 수열/수열.py
@@ -1,20 +1,31 @@
 N = int(input())
 lst = list(map(int, input().split()))
-inc_cnt = 1  # 증가하는 수열의 길이를 저장할 변수
-dec_cnt = 1  # 감소하는 수열의 길이를 저장할 변수
-result = 1   # 가장 긴 수열의 길이를 저장할 변수 (최소값은 1)
+cnt = 1    # 수열의 길이를 저장할 변수
+result = 0  # 가장 긴 수열의 길이를 저장할 변수
 
+# 커지는 수열 구하기
 for i in range(1, N):
     if lst[i-1] <= lst[i]:
-        inc_cnt += 1
+        cnt += 1
     else:
-        inc_cnt = 1  # 증가하는 수열이 끊겼을 때 초기화
+        if result < cnt:
+            result = cnt
+        cnt = 1
 
+if result < cnt:
+    result = cnt
+    
+cnt = 1
+
+# 작아지는 수열 구하기
+for i in range(1, N):
     if lst[i-1] >= lst[i]:
-        dec_cnt += 1
+        cnt += 1
     else:
-        dec_cnt = 1  # 감소하는 수열이 끊겼을 때 초기화
-
-    result = max(result, inc_cnt, dec_cnt)
+        if result < cnt:
+            result = cnt
+        cnt = 1
+if result < cnt:
+    result = cnt
 
 print(result)
