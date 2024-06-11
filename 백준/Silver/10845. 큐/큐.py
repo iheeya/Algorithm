3a1e2543d66ce5cb6d1749ec21f67a6203e6b664
hee <149504381+iheeya@users.noreply.github.com>
2024-06-11 16:45:39 +0900

    [Silver IV] Title: 큐, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())   # 명령어의 수
+queue = []
+
+for tc in range(N):
+    cmd = list(input().split())
+
+    if cmd[0] == 'push':
+        queue.append(cmd[1])
+    
+    elif cmd[0] == 'pop':
+        if len(queue) != 0:
+            print(queue.pop(0))
+        else:
+            print(-1)
+    
+    elif cmd[0] == 'size':
+        print(len(queue))
+
+    elif cmd[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    
+    elif cmd[0] == 'front':
+        if len(queue) != 0:
+            print(queue[0])
+        else:
+            print(-1)
+
+    elif cmd[0] == 'back':
+        if len(queue) != 0:
+            print(queue[-1])
+        else:
+            print(-1)
+
