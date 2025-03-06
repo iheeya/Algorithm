da5985e0122231771bfc9c9f1926984b41d18f0a
hee <149504381+iheeya@users.noreply.github.com>
2025-03-06 22:34:44 +0900

    [Bronze I] Title: 평균, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,15 +1,15 @@
-N = int(input()) #시험본 과목의 개수
+N = int(input())   # 과목의 개수
+score = list(map(float, input().split()))   # 과목의 점수를 공백을 기준으로 리스트에 저장
 
-score = list(map(int, input().split()))
-M = max(score)
+# 최고점을 기준으로 새로운 성적 만들기
 
-new_score = []
+# 최고점 구하기
+highest = max(score)
 
-for s in score:
-    new_score.append(s/M*100)
+# 새로운 성적을 만드는 식
+new_score = [0] * N
 
-n = sum(new_score)
-result = n/N
+for i in range(len(score)):
+    new_score[i] = score[i]/highest*100
 
-
-print(result)
+print(sum(new_score)/N)
