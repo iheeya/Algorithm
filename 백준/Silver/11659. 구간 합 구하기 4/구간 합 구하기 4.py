2a6d8d7722df8b35e8bb78aabfa27cf74070d96b
hee <149504381+iheeya@users.noreply.github.com>
2025-07-31 16:41:47 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 224 ms, Memory: 41144 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -2,27 +2,16 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-# N: 수의 개수
-# M: 합을 구해야 하는 횟수
+A = list(map(int, input().split()))    # 오리지널 배열
 
-A = list(map(int, input().split()))   # 원래 배열
+S = [0] * (N+1)
 
-# 구간 합 배열 만들기
-# 배열과는 달리 인덱스가 1부터 시작하므로 인덱스 0 자리에는 0 넣어 놓기
+for i in range(N):
+    S[i+1] = A[i] + S[i]
 
-S = [0]
-
-# 반복문을 통해 구간합 구하기
-temp = 0
-for n in A:
-    temp += n
-    S.append(temp)
 
 for _ in range(M):
-    i, j = map(int, input().split())   # 합을 구해야하는 구간 인덱스
-
-    # 구간 합 배열을 사용해 i번째 수부터 j 번째 수의 합 구하기
-    print(S[j] - S[i-1])
-
-
+    i, j = map(int, input().split())
+    result = S[j] - S[i-1]
+    print(result)
 
