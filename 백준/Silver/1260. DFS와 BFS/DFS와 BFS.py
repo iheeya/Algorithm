68c21d4b6e84c2d3cdb0fd16cbfa9255ed06918f
hee <149504381+iheeya@users.noreply.github.com>
2025-08-01 23:57:26 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 35676 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,65 +1,47 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-def bfs(bfs_lst):
-    global bq, visited_bfs
+N, V, I = map(int, input().split())    # 노드, 간선, 시작 노드
+visited_dfs = [False] * (N+1)
+visited_bfs = [False] * (N+1)
+A = [[] for _ in range(N+1)]   # 인접 리스트
 
-    while bq:
-        node = bq.popleft()
-        if  not visited_bfs[node]:
-            visited_bfs[node] = True
-            bfs_lst.append(node)
+# 인접 리스트 만들기
+for i in range(V):
+    u, v = map(int, input().split())
+    A[u].append(v)
+    A[v].append(u)
+
+
+# 인접 리스트 오름차순으로 정렬
+for i in range(N+1):
+    A[i].sort()
+
+def DFS(v):
+    visited_dfs[v] = True
+    print(v, end = ' ')
+    for i in A[v]:
+        if not visited_dfs[i]:
+            DFS(i)
+
+def BFS(v):
+    deq = deque()
+
+    deq.append(v)
+
+    while deq:
+        node_now = deq.popleft()
+        visited_bfs[node_now] = True
+
+        for i in A[node_now]:
+            if not visited_bfs[i]:
+                deq.append(i)
+                visited_bfs[i] = True
         
-        graph[node].sort()  # 정점이 여러개인 경우 더 작은것 부터 방문
-        for next in graph[node]:
-            if not visited_bfs[next]:
-                bq.append(next)
-    
-    return bfs_lst
+        print(node_now, end = ' ')
 
 
-def dfs(start, dfs_lst):
-    global dq, visited_dfs
-
-    # 가지치기: 이미 방문한 노드라면 종료
-    if visited_dfs[start]:
-        return
-
-    dq.append(start)
-    while dq:
-        node  = dq.popleft()
-        if not visited_dfs[node]:
-            visited_dfs[node] = True
-            dfs_lst.append(node)
-        
-        graph[node].sort()
-        for next in graph[node]:
-            dfs(next, dfs_lst)
-    
-    return dfs_lst
-
-
-
-
-N, M, V = map(int, input().split())
-# N: 정점의 개수  M: 간선의 개수  V: 탐색을 시작할 정점의 번호
-
-visited_bfs = [False] * (N+1)   # 방문체크를 위한 배열
-visited_dfs = [False] * (N+1)   # 방문체크를 위한 배열
-graph = [[] for _ in range(N + 1)]
-
-for _ in range(M):
-    a, b = map(int, input().split())   # 서로 연결된 정점들
-    graph[a].append(b)
-    graph[b].append(a)
-
-bq = deque()  # bfs에서 사용할 deque
-bq.append(V)
-
-bfs_result = bfs([])
-
-dq = deque()   # dfs에서 사용할 deque
-dfs_result = dfs(V, [])
-
-# 출력: 첫째 줄 DFS 실행 결과, 둘째 줄 BFS 실행 결과
-print(*dfs_result)
-print(*bfs_result)
+DFS(I)
+print()   # 줄 바꿈을 위한 코드
+BFS(I)
