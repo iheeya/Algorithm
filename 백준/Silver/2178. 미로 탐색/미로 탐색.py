7d21b753d51edb86cc7c5603ca47e517e2ca23db
hee <149504381+iheeya@users.noreply.github.com>
2025-09-18 17:34:30 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,39 +1,23 @@
+import sys
+input = sys.stdin.readline
 from collections import deque
 
-def bfs(x, y):  
-    global arr
-    q = deque()
-    q.append([x, y]) 
-    
+N, M = map(int, input().split())
+A = [list(map(int, input().strip())) for _ in range(N)]
 
-    arr[0][0] = 0
-    
-    # 사방 탐색을 위한 배열 
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-    while q:
-        x, y = q.popleft()
+q = deque([(0, 0)])
 
-        for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-            if 0<=nx<N and 0<=ny<M:
-                if arr[nx][ny] == 1:
-                    q.append([nx, ny])
-                    arr[nx][ny] = arr[x][y] + 1
+while q:
+    x, y = q.popleft()
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-    return arr[N-1][M-1] + 1
-                    
-   
+        if 0 <= nx < N and 0 <= ny < M and A[nx][ny] == 1:
+            A[nx][ny] += A[x][y]   # 거리계산
+            q.append((nx, ny))
 
-
-N, M = map(int, input().split())  # N: 미로의 세로 길이  M: 미로의 가로 길이 
-arr = [list(map(int, input())) for _ in range(N)]  # 미로
-graph = []
-
-
-result = bfs(0,0)
-
-# 출력: 도착지까지 지나야 하는 칸 수
-print(result)
+print(A[N-1][M-1])
