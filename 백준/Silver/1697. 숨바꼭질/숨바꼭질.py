787f7e74fa06e84c5c2619d684f71fec835772f5
hee <149504381+iheeya@users.noreply.github.com>
2025-09-19 22:45:11 +0900

    [Silver I] Title: 숨바꼭질, Time: 128 ms, Memory: 38164 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,31 +1,24 @@
+import sys
+input = sys.stdin.readline
 from collections import deque
 
-def bfs(N, K):
-    global visited
-    q = deque()
-    q.append(N)
-
-    while q:
-        x = q.popleft()
-
-        # 현재 방문 노드가 동생의 위치와 같다면
-        if x == K:
-            print(visited[x])
-            break
-        
-        for nx in (x+1, x-1, x*2):
-            if 0<=nx<=100000 and not visited[nx]:
-                visited[nx] = visited[x]+1
-                q.append(nx)
-
-
-
 N, K = map(int, input().split())
-# N: 수빈이의 위치
-# K: 동생의 위치
+MAX = 10 ** 5
+A = [0] * (MAX + 1)
+q= deque([N])
 
-visited = [0] *100001
+while q:
+    x = q.popleft()
 
-bfs(N, K)
+    if x == K:
+        break
 
-# 출력: 수빈이가 동생을 찾을 수 있는 가장 빠른 시간
+    for nx in (x-1, x+1, x*2):
+        if 0 <= nx <= MAX and not A[nx]:   # nx를 아직 방문하지 않았을 때만 큐에 삽입
+            A[nx]  = A[x] + 1
+            q.append(nx)
+
+
+
+print(A[K])
+    
